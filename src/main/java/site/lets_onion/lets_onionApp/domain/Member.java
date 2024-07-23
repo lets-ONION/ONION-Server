83d6ce4e8206a52865dcd1c5be48f15a0f58b98a
ShinSub98 <wjdtjq26@naver.com>
2024-07-23 20:46:31 +0900

    <feat>:added Member entity

---
+++ src/main/java/site/lets_onion/lets_onionApp/domain/Member.java
@@ -0,0 +1,29 @@
+package site.lets_onion.lets_onionApp.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity @Getter
+@NoArgsConstructor
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String kakaoId;
+    private String nickname;
+
+
+    @Builder
+    public Member(@NonNull String kakaoId) {
+        this.kakaoId = kakaoId;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}

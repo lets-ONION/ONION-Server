7e9f8b960fa76810c34cc0209e8eaca75753100a
Chaeyoung714 <202102760@hufs.ac.kr>
2024-07-31 17:38:04 +0900

    feat: 유저의 특정 onion 조회하는 로직 수정

--- src/main/java/site/lets_onion/lets_onionApp/service/trade/TradeServiceImpl.java
+++ src/main/java/site/lets_onion/lets_onionApp/service/trade/TradeServiceImpl.java
@@ -5,10 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.lets_onion.lets_onionApp.domain.onionBook.Onion;
+import site.lets_onion.lets_onionApp.domain.onionBook.OnionBook;
 import site.lets_onion.lets_onionApp.domain.trade.TradeRequest;
 import site.lets_onion.lets_onionApp.domain.trade.TradeStatus;
 import site.lets_onion.lets_onionApp.repository.onionBook.OnionBookRepository;
-import site.lets_onion.lets_onionApp.repository.onionBook.OnionRepository;
 import site.lets_onion.lets_onionApp.repository.trade.TradeRepository;
 
 @Service
@@ -17,19 +17,20 @@ import site.lets_onion.lets_onionApp.repository.trade.TradeRepository;
 public class TradeServiceImpl implements TradeService{
 
     private final BaseMemberRepository memberRepository;
-    private final OnionRepository onionRepository;
     private final TradeRepository tradeRepository;
     private final OnionBookRepository onionBookRepository;
 
     /**
      * 검증
      */
+    // 교환 가능한지 체크
     public void validateOnionQuantity(Onion onion) {
 
         if (onion.getCollectedQuantity() <= 1) {
             throw new NotEnoughQuantityException("양파 개수가 부족합니다.");
         }
     }
+    //중복된 요청 체크
     public void validateDuplicateRequest(TradeRequest tradeRequest) {
 
         if (tradeRequest.getStatus() != TradeStatus.PENDING) {
@@ -47,9 +48,11 @@ public class TradeServiceImpl implements TradeService{
         TradeRequest tradeRequest = new TradeRequest(fromMember, toMember, fromOnionType, toOnionType);
 
         // 교환 요청한 유저의 교환 요청 양파 개수 -1
-        Onion fromOnionOfFromMember = onionRepository.findByMemberIdAndOnionType(fromMemberId, fromOnionType);
+        OnionBook fromMemberOnionBook = onionBookRepository.findByMemberId(fromMemberId);
+        Onion fromOnionOfFromMember = fromMemberOnionBook.getOnion(fromOnionType);
+
         validateOnionQuantity(fromOnionOfFromMember);
-        fromOnionOfFromMember.decreaseQuantity(); //이런애들 저장 안해도 되나?
+        fromOnionOfFromMember.decreaseQuantity();
 
         tradeRepository.save(tradeRequest);
 
@@ -59,16 +62,18 @@ public class TradeServiceImpl implements TradeService{
     @Override
     @Transactional
     public void cancelRequest(Long tradeId) {
-
+        //엔티티 조회
         TradeRequest tradeRequest = tradeRepository.findById(tradeId);
 
         validateDuplicateRequest(tradeRequest);
-        tradeRequest.updateStatus(TradeStatus.CANCEL); //저장 안해도 되나?
+        tradeRequest.updateStatus(TradeStatus.CANCEL);
 
         OnionType fromOnionType = tradeRequest.getFromOnion();
         Member fromOnionMember = tradeRequest.getFromMember();
 
-        Onion fromOnionOfFromMember = onionRepository.findByMemberIdAndOnionType(fromOnionMember.getId(), fromOnionType);
+        //교환 신청한 유저의 교환 요청 양파 개수 원복
+        OnionBook fromMemberOnionBook = onionBookRepository.findByMemberId(fromOnionMember.getId());
+        Onion fromOnionOfFromMember = fromMemberOnionBook.getOnion(fromOnionType);
         fromOnionOfFromMember.increaseQuantity();
 
     }
@@ -76,7 +81,7 @@ public class TradeServiceImpl implements TradeService{
     @Override
     @Transactional
     public void acceptRequest(Long tradeId) {
-
+        //엔티티 조회
         TradeRequest tradeRequest = tradeRepository.findById(tradeId);
 
         validateDuplicateRequest(tradeRequest);
@@ -87,14 +92,17 @@ public class TradeServiceImpl implements TradeService{
         Member fromOnionMember = tradeRequest.getFromMember();
         Member toOnionMember = tradeRequest.getToMember();
 
-        Onion toOnionOfToMember = onionRepository.findByMemberIdAndOnionType(toOnionMember.getId(), toOnionType);
+        OnionBook fromMemberOnionBook = onionBookRepository.findByMemberId(fromOnionMember.getId());
+        OnionBook toMemberOnionBook = onionBookRepository.findByMemberId(toOnionMember.getId());
+
+        Onion toOnionOfToMember = toMemberOnionBook.getOnion(toOnionType);
         validateOnionQuantity(toOnionOfToMember);
         toOnionOfToMember.decreaseQuantity();
 
-        Onion fromOnionOfToMember = onionRepository.findByMemberIdAndOnionType(toOnionMember.getId(), fromOnionType);
+        Onion fromOnionOfToMember = toMemberOnionBook.getOnion(fromOnionType);
         fromOnionOfToMember.increaseQuantity();
 
-        Onion toOnionOfFromMember = onionRepository.findByMemberIdAndOnionType(fromOnionMember.getId(), toOnionType);
+        Onion toOnionOfFromMember = fromMemberOnionBook.getOnion(toOnionType);
         toOnionOfFromMember.increaseQuantity();
 
     }
@@ -102,7 +110,7 @@ public class TradeServiceImpl implements TradeService{
     @Override
     @Transactional
     public void refuseRequest(Long tradeId) {
-
+        //엔티티 조회
         TradeRequest tradeRequest = tradeRepository.findById(tradeId);
 
         validateDuplicateRequest(tradeRequest);
@@ -111,7 +119,9 @@ public class TradeServiceImpl implements TradeService{
         OnionType fromOnionType = tradeRequest.getFromOnion();
         Member fromOnionMember = tradeRequest.getFromMember();
 
-        Onion fromOnionOfFromMember = onionRepository.findByMemberIdAndOnionType(fromOnionMember.getId(), fromOnionType);
+        //교환 신청한 유저의 교환 요청 양파 개수 원복
+        OnionBook fromMemberOnionBook = onionBookRepository.findByMemberId(fromOnionMember.getId());
+        Onion fromOnionOfFromMember = fromMemberOnionBook.getOnion(fromOnionType);
         fromOnionOfFromMember.increaseQuantity();
 
     }

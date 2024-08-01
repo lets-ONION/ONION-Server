93ce3f919a12408eb3572f692b10b214b23f5a33
Zena0128 <0128ja@gmail.com>
2024-08-01 23:18:34 +0900

    feat: 역직렬화 필요한 DTO에 @NoArgsConstructor 추가

--- src/main/java/site/lets_onion/lets_onionApp/dto/calendar/PosNoteDTO.java
+++ src/main/java/site/lets_onion/lets_onionApp/dto/calendar/PosNoteDTO.java
@@ -1,8 +1,10 @@
 package site.lets_onion.lets_onionApp.dto.calendar;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class PosNoteDTO {
 
     private String note;

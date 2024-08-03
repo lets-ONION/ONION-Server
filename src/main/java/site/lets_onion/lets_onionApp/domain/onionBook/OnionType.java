a8b3d99ee32aea910fce1eca4431b206f0318707
Chaeyoung714 <202102760@hufs.ac.kr>
2024-08-03 19:26:43 +0900

    hotfix: oniontype image url에 .png 붙임

--- src/main/java/site/lets_onion/lets_onionApp/domain/onionBook/OnionType.java
+++ src/main/java/site/lets_onion/lets_onionApp/domain/onionBook/OnionType.java
@@ -9,16 +9,16 @@ import java.util.Random;
 @Getter
 public enum OnionType {
 
-    ONION_GGANG("양파깡", "https://imgur.com/s8D57zC"),
-    ONION_RING("양파링", "https://imgur.com/vfcZzfs"),
-    ONION_RAW("생양파", "https://imgur.com/cpH5jvA"),
-    ONION_PILLED("깐양파", "https://imgur.com/dNv03Iq"),
-    ONION_FRIED("양파튀김", "https://imgur.com/NNg1jfT"),
-    ONION_PICKLE("양파피클", "https://imgur.com/4mjrwWX"),
-    ONION_SUSHI("양파가 누운 연어초밥", "https://imgur.com/rHv8v3c"),
-    ONION_KIMCHI("양파김치", "https://imgur.com/0TlJGFL"),
-    ONION_SOUP("양파스프", "https://imgur.com/ffjeUVx"),
-    ONION_GRILLED("구운 양파", "https://imgur.com/duzJbpC");
+    ONION_GGANG("양파깡", "https://imgur.com/s8D57zC.png"),
+    ONION_RING("양파링", "https://imgur.com/vfcZzfs.png"),
+    ONION_RAW("생양파", "https://imgur.com/cpH5jvA.png"),
+    ONION_PILLED("깐양파", "https://imgur.com/dNv03Iq.png"),
+    ONION_FRIED("양파튀김", "https://imgur.com/NNg1jfT.png"),
+    ONION_PICKLE("양파피클", "https://imgur.com/4mjrwWX.png"),
+    ONION_SUSHI("양파가 누운 연어초밥", "https://imgur.com/rHv8v3c.png"),
+    ONION_KIMCHI("양파김치", "https://imgur.com/0TlJGFL.png"),
+    ONION_SOUP("양파스프", "https://imgur.com/ffjeUVx.png"),
+    ONION_GRILLED("구운 양파", "https://imgur.com/duzJbpC.png");
 
 
     private final String onionName;
